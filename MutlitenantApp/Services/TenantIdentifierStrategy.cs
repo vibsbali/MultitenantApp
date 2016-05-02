23cf54bd35bfda8103c9f831d4f9eb7604c8f13a
Vaibhav Bali <vbali@internode.on.net>
2016-05-02 16:11:36 +1000

    Implemented TenantIdentifierStrategy

---
+++ MutlitenantApp/Services/TenantIdentifierStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Routing;
+
+namespace MutlitenantApp.Services
+{
+    class TenantIdentifierStrategy : ITenantIdentifierStrategy
+    {
+        public string GetCurrentTenant(RequestContext context)
+        {
+            if (context.HttpContext.Request.Url != null) return context.HttpContext.Request.Url.Host.ToLower();
+        }
+    }
+}
